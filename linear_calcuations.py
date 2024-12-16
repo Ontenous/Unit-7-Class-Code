8b05f9947532b210216b22fef6a1368fd32a6622
Ontenous <nathanw@wlhs.wlwv.k12.or.us>
2024-12-16 09:22:46 -0800

    completed assignments

--- linear_calcuations.py
+++ linear_calcuations.py
@@ -1,17 +1,23 @@
 """
-Name:
-Date:
-Description:
+Name:William Nathan
+Date: 12/16/24
+Description: Slope project
 """
 
 def slope(point1:tuple,point2:tuple)->float:
-    return 0.0
+    rise = float(point1[1]-point2[1])
+    run = float(point1[0]-point2[0])
+    slope = rise/run
+    return slope
 
 def distance(point1:tuple, point2:tuple)->float:
-    return 0.0
+    distance = float(point2[0]-point1[0])
+    return distance
 
 def main():
-    pass
+    print(slope((1,1),(2,2)))
+    print(distance((1,1),(2,2)))
+
 
 if __name__ == "__main__":
     main()
--- tuple_lesson.py
+++ tuple_lesson.py
@@ -1,6 +1,6 @@
 """
-Name:
-Date:
+Name: William Nathan
+Date: 12/16/24
 Lesson: Tuples
 """
 
@@ -11,22 +11,28 @@ I pronounce tuple too-pull on Mon/Wed/Fri and tub-pull on Tue/Thu/Sat.
 On Sunday I don't talk about them. :) 
 """
 # The look like an ordered pair in math
-
 # Make tuple with two items
+my_tuple = ("3",4)
+print(my_tuple)
 
 # Make tuple with one item
-
+lonely_tuple = (5,) # you have to have both () and the ,
+print(lonely_tuple)
 
 # You access elements from them using [ ] just like strings
-
-
+print(my_tuple[0])
 
 # Also like strings, you can only concatenate tuples with other tuples
+not_lonely_tuple = my_tuple + (5,)
+print(not_lonely_tuple)
 
+integer_tuple = (int(my_tuple[0]),4,5)
+print(integer_tuple)
 # I heard you like tuples...
 # so you can also put tuples inside your tuples
 # need two [] to access them.
-
+character_info = (("Rosalie",100),("Cedric",75),("Shana",94))
+print(character_info[1][0]) # return "Cedric"
 
 ########## Exercise ##########
 
