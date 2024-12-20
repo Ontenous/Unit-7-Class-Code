e018e4bab3cec39f14c6c54fbc683ece8689d04c
Ontenous <nathanw@wlhs.wlwv.k12.or.us>
2024-12-20 08:57:30 -0800

    forgot how to do math

--- linear_calcuations.py
+++ linear_calcuations.py
@@ -3,16 +3,19 @@ Name:William Nathan
 Date: 12/16/24
 Description: Slope project
 """
+import math
 
 def slope(point1:tuple,point2:tuple)->float:
-    rise = float(point1[1]-point2[1])
-    run = float(point1[0]-point2[0])
+    rise = float(point2[1]-point1[1])
+    run = float(point2[0]-point1[0])
     slope = rise/run
     return slope
 
 def distance(point1:tuple, point2:tuple)->float:
-    distance = float(point2[0]-point1[0])
-    return distance
+    deltax = float(point2[0]-point1[0])
+    deltay = float(point2[1]-point1[1])
+    pythag = math.sqrt((deltay*deltay) + (deltax*deltax))
+    return pythag
 
 def main():
     print(slope((1,1),(2,2)))
